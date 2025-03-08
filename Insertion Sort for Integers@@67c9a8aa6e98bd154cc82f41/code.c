3a8fa6c72e44522d570c0c0f6a02699188cb3871
PrabhjotShergill <prabhjotshergill26@gmail.com>
2025-03-08 17:49:39 +0530

    Solution to Insertion Sort for Integers

--- Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -4,7 +4,7 @@ void swap(int *a, int *b){
     *b = temp;
 }
 
-void bubbleSort(int arr[], int n){
+void insertionSort(int arr[], int n){
     for(int i=0; i<n-1; i++){
         for(int j=0; j<n-1; j++){
             if(arr[j] > arr[j+1]){
