816d695fd502a40269466fa9fc0da53d4e67b158
PrabhjotShergill <prabhjotshergill26@gmail.com>
2025-03-30 13:20:55 +0530

    Solution to Find the Maximum Product of Two Elements in an Array

--- Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
+++ Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
@@ -10,7 +10,7 @@ int main(){
     int prod=0;
 
     for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
+        for(int j=1; j<n; j++){
             if(arr[i] * arr[j] > prod){
                 prod = arr[i]*arr[j];
             }
